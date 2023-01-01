3a10640f11422bd3b968ad54c501eb63e2b29b5f
DavidWindham <tinymouse2@gmail.com>
2023-01-01 10:55:26 +0000

    Added another page and a component

---
+++ components/test_component.tsx
@@ -0,0 +1,7 @@
+const TestComponent = () => {
+    return <div>
+        <h1>Component has been loaded in</h1>
+    </div>
+}
+
+export default TestComponent
---
+++ pages/folder/other-page.tsx
@@ -0,0 +1,15 @@
+import TestComponent from "../../components/test_component";
+import Link from 'next/link';
+
+export default function OtherPage() {
+    return <>
+      <TestComponent />
+      <h1 className="title">
+        Other Page
+      </h1>
+      <h1>
+        Link <Link href="/">this page!</Link>
+        A <a href="/">this page!</a>
+      </h1>
+    </>
+}
